cac20a753e8a13e2e2a6c2dc80b0bce06d834cec
Hysware <56950515+Hysware@users.noreply.github.com>
2025-12-09 22:36:37 +0800

    root2m4b

---
+++ tran.cc
@@ -0,0 +1,44 @@
+// tran.cc ---
+//
+// Description:
+// Author: Hongyi Wu(吴鸿毅)
+// Email: wuhongyi@qq.com
+// Created: 日 6月  3 09:05:30 2018 (+0800)
+// Last-Updated: 一 1月 17 19:46:53 2022 (+0800)
+//           By: Hongyi Wu(吴鸿毅)
+//     Update #: 4
+// URL: http://wuhongyi.cn
+
+void help()
+{
+ std:cout<<"root2m4b(const char *rootfilename,const char *m4bfilename)"<<std::endl;
+}
+
+void root2m4b(const char *rootfilename,const char *m4bfilename)
+{
+  TFile *file = new TFile(rootfilename,"READ");//"RECREATE" "READ"
+  if(!file->IsOpen())
+    {
+      std::cout<<"Can't open root file"<<std::endl;
+    }//const char *rootfilename,const char *m4bfilename；
+
+  TH2I *matrix = (TH2I*)file->Get("mat_all_prompt");
+
+
+  // write matrix file
+  FILE *fp;
+  if((fp=fopen(m4bfilename,"ab")) == NULL)
+    std::cout<<"can't open file .m4b"<<std::endl;
+
+  int count;
+  for (int i = 1; i <= 4096; ++i)
+    for (int j = 1; j <= 4096; ++j)
+      {
+    count = int(matrix->GetBinContent(i, j));
+    fwrite(&count,sizeof(int),1,fp);
+      }
+
+  fclose(fp);
+
+  file->Close();
+}
